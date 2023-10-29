df215b0b6b9efc805f17296f75dcf075f65faa10
EnkhjinicoZHC <eenkhjinico.gmail.com>
2023-10-29 22:52:35 +0800

    Added Heroku Config

--- Chefly/settings.py
+++ Chefly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^s56@*w*km=46)q7@!-l1uxerlunwl17(uxhe@=ttnbiir8y(p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
